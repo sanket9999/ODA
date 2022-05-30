959b42c7a033a34e5ceba5053eac5ecf684d3caa
Sanket <sknangare@mitaoe.ac.in>
2022-05-30 22:42:45 +0530

    .

--- index.php
+++ index.php
@@ -139,7 +139,7 @@ function checklen()
                                         </div>
                                         
                                         <div class="form-group">
-                                            <input type="tel" minlength="10" maxlength="10" name="contact" class="form-control" placeholder="Your Phone *"  />
+                                            <input type="number" minlength="10" maxlength="10" name="contact" class="form-control" placeholder="Your Phone *"  />
                                         </div>
                                         <div class="form-group">
                                             <input type="password" class="form-control"  id="cpassword" placeholder="Confirm Password *" name="cpassword"  onkeyup='check();' required/><span id='message'></span>
@@ -199,7 +199,7 @@ function checklen()
                             </div>
                             <div class="tab-pane fade show" id="Pharmacist" role="tabpanel" aria-labelledby="profile-tab">
                                 <h3  class="register-heading">Login as Pharmacist</h3>
-                                <form method="post" action="func3.php">
+                                <form method="post" action="pharmacy-panel.php">
                                 <div class="row register-form">
                                     <div class="col-md-6">
                                         <div class="form-group">
@@ -222,7 +222,7 @@ function checklen()
                         </div>
                         <div class="tab-pane fade show" id="Pathologist" role="tabpanel" aria-labelledby="profile-tab">
                                 <h3  class="register-heading">Login as Pathologist</h3>
-                                <form method="post" action="func3.php">
+                                <form method="post" action="Pathology-panel.php">
                                 <div class="row register-form">
                                     <div class="col-md-6">
                                         <div class="form-group">
@@ -253,6 +253,5 @@ function checklen()
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
-    </html>
+</html>
 
-  
